58303417bcdc3925b6380aaaa3c2c246b3c0cf58
Vitalik-11 <vitalydubchinsky@gmail.com>
2024-07-13 20:05:14 +0200

    init commit

---
+++ new.one.py
@@ -0,0 +1,24 @@
+from inspect import isgenerator
+
+
+def pow(x):
+    return x ** 2
+
+
+def some_gen(begin, end, func):
+    """
+     begin: перший елемент послідовності
+     end: кількість елементів у послідовності
+     func: функція, яка формує значення для послідовності
+    """
+    was = begin
+    yield was
+    for a in range(begin, end + 1):
+        was = func(was)
+        yield was
+
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
